f8c74a859f00b191f866874d13cdfa70b1c6c5c4
ayoolaayowale91-creator <ayoolaayowale91@gmail.com>
2026-05-26 08:56:00 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  botName: "Admin Bot",
+  prefix: "!",
+  ownerNumber: "2349016105277", // Replace with your WhatsApp number
+  ownerName: "Owner",
+  botVersion: "1.0.0",
+}
+
+module.exports = config
